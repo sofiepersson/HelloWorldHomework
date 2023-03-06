792ee1dcec9ad5cb6d9687b3e53fa1834b356e79
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2023-03-06 20:18:25 +0100

    Finishes String with All Letters Assignment

--- StringWithAllLetters/StringWithAllLetters/StringWithAllLetters.cpp
+++ StringWithAllLetters/StringWithAllLetters/StringWithAllLetters.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <array>
 
+using namespace std;
+
+int main() {
+	char alphabet[27]{}; // 26 letters in the alphabet + 1 null terminator
+	int arraySize = size(alphabet);
+	for (int i = 0; i < arraySize - 1; ++i) { // -1 to not change the null terminator
+		alphabet[i] = 'A' + i;
+	}
+	cout << alphabet << endl;
+	return 0;
+}
